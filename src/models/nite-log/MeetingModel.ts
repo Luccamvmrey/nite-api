3518e73c7314aae54fc077ea5ce95b2cc1366e39
Iago Carvalho <raposafoxkitsunezorrorenard071@gmail.com>
2025-03-05 14:24:40 -0300

    fix create meeting

--- src/models/nite-log/MeetingModel.ts
+++ src/models/nite-log/MeetingModel.ts
@@ -11,7 +11,8 @@ export default class MeetingModel {
         return Meeting.create({
             data: {
                 meetingCode: createRandomMeetingCode(),
-                date: formattedDate
+                date: formattedDate,
+                attendanceList: {}
             }
         })
     }
